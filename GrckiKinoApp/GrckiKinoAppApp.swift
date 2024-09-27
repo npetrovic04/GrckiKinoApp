d475de0e29b2e96cf7faf732209e31ddad6cfe73
npetrovic04 <npetrovic04@yahoo.com>
2024-09-28 00:54:13 +0200

    Initial Commit

---
+++ GrckiKinoApp/GrckiKinoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrckiKinoAppApp.swift
+//  GrckiKinoApp
+//
+//  Created by Nenad Petrovic on 28.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct GrckiKinoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GrckiKinoAppTests/GrckiKinoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GrckiKinoAppTests.swift
+//  GrckiKinoAppTests
+//
+//  Created by Nenad Petrovic on 28.09.2024.
+//
+
+import Testing
+@testable import GrckiKinoApp
+
+struct GrckiKinoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
