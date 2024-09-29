47bf87c5c70cad0f96224a4900fc14816b925123
npetrovic04 <npetrovic04@yahoo.com>
2024-09-29 21:47:22 +0200

    Kreirana lista id i time

---
+++ GrckiKinoApp/KinoViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  KinoViewModel.swift
+//  GrckiKinoApp
+//
+//  Created by Nenad Petrovic on 28.09.2024.
+//
+
+import Foundation
---
+++ GrckiKinoApp/Kolo.swift
@@ -0,0 +1,8 @@
+//
+//  Kolo.swift
+//  GrckiKinoApp
+//
+//  Created by Nenad Petrovic on 28.09.2024.
+//
+
+import Foundation
