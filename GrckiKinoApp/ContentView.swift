c31b91642ce47cfbd88eca7c255969aa2220a6da
npetrovic04 <npetrovic04@yahoo.com>
2024-09-30 00:35:05 +0200

    dodate nove funkcije

--- GrckiKinoApp/ContentView.swift
+++ GrckiKinoApp/ContentView.swift
@@ -17,6 +17,13 @@ struct ContentView: View {
                 VStack(alignment: .leading) {
                     Text("Kolo ID: \(kolo.id)")
                     Text("Vreme izvlačenja: \(kolo.drawTime)")
+                    
+                    // Prikaz tajmera
+                    if let timeRemaining = viewModel.countdowns[kolo.id], timeRemaining > 0 {
+                        Text("Preostalo vreme: \(timeRemaining)")
+                    } else {
+                        Text("Izvlacenje je zavrseno")
+                    }
                 }
             }
             .navigationTitle("Grčki Kino - Kola")
@@ -24,6 +31,20 @@ struct ContentView: View {
                 viewModel.fetchUpcomingGames()
             }
         }
+    }
+    
+    // Pomoćna funkcija za formatiranje vremena izvlačenja
+    func formattedDate(_ date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss"
+        return formatter.string(from: date)
+    }
+    
+    // Pomoćna funkcija za formatiranje preostalog vremena (sekunde)
+    func formattedTime(_ time: TimeInterval) -> String {
+        let minutes = Int(time) / 60
+        let seconds = Int(time) % 60
+        return String(format: "%02d:%02d", minutes, seconds)
     }
 }
 
