83e231df559ef50f9a1a9afa7fa95b1d7800eeba
npetrovic04 <npetrovic04@yahoo.com>
2024-10-01 01:23:35 +0200

    Dodat talon

---
+++ GrckiKinoApp/TalonView.swift
@@ -0,0 +1,18 @@
+//
+//  TalonView.swift
+//  GrckiKinoApp
+//
+//  Created by Nenad Petrovic on 01.10.2024.
+//
+
+import SwiftUI
+
+struct TalonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TalonView()
+}
